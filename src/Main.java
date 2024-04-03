24ea9692dfc67df610ab0aed748bc3a8a756b77f
alkaz07 <alkaz07@yandex.ru>
2024-04-03 11:32:24 +0300

    добавлена улучшенная сортировка

--- src/Main.java
+++ src/Main.java
@@ -27,4 +27,22 @@ public class Main {
             }
         }
     }
+
+    public static void bubbleSortImproved(int[] array){
+        int swapCounter;
+        for (int i = 0; i < array.length-1; i++) {
+            swapCounter=0;
+            for (int j = 0; j < array.length-1-i; j++) {
+                if (array[j] > array[j+1])
+                {//поменять местами
+                    int t = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = t;
+                    swapCounter++;
+                }
+            }
+            if (swapCounter == 0)
+                break;
+        }
+    }
 }
